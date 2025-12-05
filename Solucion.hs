8209839f6cb1913c4f1f2772bb251ea7e99b58e2
Mauricio Quintero <Mauricio.Quintero@fi365.ort.edu.uy>
2025-12-04 23:50:34 -0300

    1.2 verifyA

--- Solucion.hs
+++ Solucion.hs
@@ -1,10 +1,41 @@
 --Teoria de la computacion - Semestre 2 - Año 2025 
---Tarea Final - Mauricio Quintero() y Oliver Sosa(224254)
+--Tarea Final - Mauricio Quintero(167070) y Oliver Sosa(224254)
 
-Problemas a estudiar: 
+module Solucion where
 
-A) SAT 
+-- 1.1 Representaci´on de dominios y soluciones
 
+-- lista exterior -> conjunciones
+-- lista interior -> clausulas
+-- tupla -> nombre variable, true indica directo, false que está negada
+type DomA = [[(String, Bool)]]
+type SolA = [(String, Bool)]
 
+-- Abajo explicaciones
+type DomB = (Ps, D, R, E, B, M, V)
+type SolB = Ps
 
-B) 
+-- punto de control (secuencial desde 1)
+type P = Int
+-- puntos de control
+type Ps = [P]
+-- matriz distancias.
+type D = [(P, P, Int)]
+-- distancias a la base
+type R = [(P, Int)]
+-- pares de exlusión
+type E = [(P, P)]
+-- prioridades de los puntos
+type B = [(P, Int)]
+-- límite de distancia
+type M = Int
+-- prioridad mínima requerida
+type V = Int
+
+-- 1.2 Verificadores en tiempo polinomial
+
+verifyA :: (DomA, SolA) -> Bool
+verifyA (lc, s) = foldr (\c->) True lc
+
+verifyB :: (DomB, SolB) -> Bool
+verifyB (d, s) = undefined
